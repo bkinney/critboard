fab369238fd4486019151890fadbacfba445df0a
bkinney <bkinney@udel.edu>
2016-08-01 13:34:52 -0400

    add text secret to replace db lookup

--- canvas_lti/critboard/index.php
+++ canvas_lti/critboard/index.php
@@ -8,13 +8,14 @@ putgrades.php
 image_error.php
 */
 session_start();
-include "/www/canvas/sitepaths.php";
+include "../sitepaths.php";
  $me = $canvashtml . "critboard/index.php";
 $tokentype="context";	 
 error_reporting(E_ERROR & ~E_WARNING);
 ini_set('display_errors', 1);
 $testing = false;
-$secret = array("table"=>"blti_keys","key_column"=>"oauth_consumer_key","secret_column"=>"secret","context_column"=>"context_id");
+$secret="yoursecrethere";
+//$secret = array("table"=>"blti_keys","key_column"=>"oauth_consumer_key","secret_column"=>"secret","context_column"=>"context_id");
 $include= $canvasphp . "critboard/common.php";
 include $canvasphp . "all_purpose.php";
 	
